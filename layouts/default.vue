1faac5bacede97175ca684b576e9e63218a8fc44
KKVAISHNAVI <87420570+KKVAISHNAVI@users.noreply.github.com>
2021-09-29 15:45:26 +0530

    Initial Code Setup

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+<style scoped>
+.v-application {
+  background-image: radial-gradient(circle, #7ad9ec, #67ccef, #60bdf1, #67adee, #789be7);
+}
+</style>
